877631700509b1379ed2585d07228a3a9554606f
Linus Tolke <linus@lysator.liu.se>
1991-08-20 15:58:17 +0000

    lyskom-do-when-done initieras av user-arean ocks}.

--- src/flags.el
+++ src/flags.el
@@ -206,6 +206,7 @@ If successful then set the buffer not-modified. Else print a warning."
 	  (++ r))
 
 	(setq lyskom-do-when-starting kom-do-when-starting)
+	(setq lyskom-do-when-done kom-do-when-done)
 	;; Remove not found user-areas
 	(let ((pos lyskom-other-clients-user-areas))
 	  (if pos
