c2a0e13a796a22fab3133602e63c664027d4d9ae
Linus Tolke <linus@lysator.liu.se>
1991-08-20 21:45:11 +0000

    Bugfix kom-do-when-done.

--- src/flags.el
+++ src/flags.el
@@ -206,7 +206,7 @@ If successful then set the buffer not-modified. Else print a warning."
 	  (++ r))
 
 	(setq lyskom-do-when-starting kom-do-when-starting)
-	(setq lyskom-do-when-done kom-do-when-done)
+	(setq lyskom-do-when-done (cons kom-do-when-done kom-do-when-done))
 	(run-hooks 'lyskom-login-hook)
 	;; Remove not found user-areas
 	(let ((pos lyskom-other-clients-user-areas))
