f07c75ba3612c3db9af0a1b16dfb852bc4b4e5a7
Linus Tolke <linus@lysator.liu.se>
1991-09-15 10:00:36 +0000

    Added copyright note

--- src/commands1.el
+++ src/commands1.el
@@ -1,3 +1,27 @@
+;;;;;
+;;;;; $Id: commands1.el,v 35.5 1991-09-15 10:06:25 linus Exp $
+;;;;; Copyright (C) 1991  Lysator Academic Computer Association.
+;;;;;
+;;;;; This file is part of the LysKOM server.
+;;;;; 
+;;;;; LysKOM is free software; you can redistribute it and/or modify it
+;;;;; under the terms of the GNU General Public License as published by 
+;;;;; the Free Software Foundation; either version 1, or (at your option) 
+;;;;; any later version.
+;;;;; 
+;;;;; LysKOM is distributed in the hope that it will be useful, but WITHOUT
+;;;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;;;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;;;;; for more details.
+;;;;; 
+;;;;; You should have received a copy of the GNU General Public License
+;;;;; along with LysKOM; see the file COPYING.  If not, write to
+;;;;; Lysator, c/o ISY, Linkoping University, S-581 83 Linkoping, SWEDEN,
+;;;;; or the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
+;;;;; MA 02139, USA.
+;;;;;
+;;;;; Please mail bug reports to bug-lyskom@lysator.liu.se. 
+;;;;;
 ;;;; ================================================================
 ;;;; ================================================================
 ;;;;
@@ -9,7 +33,7 @@
 
 (setq lyskom-clientversion-long 
       (concat lyskom-clientversion-long
-	      "$Id: commands1.el,v 35.4 1991-09-08 21:17:26 linus Exp $\n"))
+	      "$Id: commands1.el,v 35.5 1991-09-15 10:06:25 linus Exp $\n"))
 
 
 ;;; ================================================================
--- src/internal.el
+++ src/internal.el
@@ -1,3 +1,27 @@
+;;;;;
+;;;;; $Id: internal.el,v 35.4 1991-09-15 10:05:39 linus Exp $
+;;;;; Copyright (C) 1991  Lysator Academic Computer Association.
+;;;;;
+;;;;; This file is part of the LysKOM server.
+;;;;; 
+;;;;; LysKOM is free software; you can redistribute it and/or modify it
+;;;;; under the terms of the GNU General Public License as published by 
+;;;;; the Free Software Foundation; either version 1, or (at your option) 
+;;;;; any later version.
+;;;;; 
+;;;;; LysKOM is distributed in the hope that it will be useful, but WITHOUT
+;;;;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;;;;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;;;;; for more details.
+;;;;; 
+;;;;; You should have received a copy of the GNU General Public License
+;;;;; along with LysKOM; see the file COPYING.  If not, write to
+;;;;; Lysator, c/o ISY, Linkoping University, S-581 83 Linkoping, SWEDEN,
+;;;;; or the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
+;;;;; MA 02139, USA.
+;;;;;
+;;;;; Please mail bug reports to bug-lyskom@lysator.liu.se. 
+;;;;;
 ;;;; ================================================================
 ;;;; ================================================================
 ;;;;
@@ -13,7 +37,7 @@
 
 (setq lyskom-clientversion-long 
       (concat lyskom-clientversion-long
-	      "$Id: internal.el,v 35.3 1991-09-04 15:22:58 linus Exp $\n"))
+	      "$Id: internal.el,v 35.4 1991-09-15 10:05:39 linus Exp $\n"))
 
 
 ;;;; ================================================================
