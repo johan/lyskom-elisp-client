ef4faf47293f0224882734fca96a3d7ebabf2655
David Byers <byers@lysator.liu.se>
1995-03-04 14:16:06 +0000

    Fixed bugs in commands1.el, view-text.el. Moved macro from services.el to macros.el.

--- src/macros.el
+++ src/macros.el
@@ -1,5 +1,5 @@
 ;;;;;
-;;;;; $Id: macros.el,v 38.0 1994-01-06 01:58:27 linus Exp $
+;;;;; $Id: macros.el,v 38.1 1995-03-04 14:16:10 byers Exp $
 ;;;;; Copyright (C) 1991  Lysator Academic Computer Association.
 ;;;;;
 ;;;;; This file is part of the LysKOM server.
@@ -31,7 +31,7 @@
 ;;;; be compiled.
 ;;;;
 
-(defconst lyskom-clientversion-long "$Id: macros.el,v 38.0 1994-01-06 01:58:27 linus Exp $\n"
+(defconst lyskom-clientversion-long "$Id: macros.el,v 38.1 1995-03-04 14:16:10 byers Exp $\n"
   "Version for every file in the client.")
 
 
@@ -85,6 +85,52 @@ Value returned is always nil."
 (defmacro -- (var)
   "Decrement the variable VAR and return the value."
   (list 'setq var (list '1- var)))
+
+
+;; Multiple blocking read from server
+
+(defvar lyskom-multiple-blocking-return nil
+  "Return from blocking-do-multiple")
+
+(defun lyskom-blocking-do-multiple (call-list)
+  (let ((lyskom-multiple-blocking-return 'not-yet-gotten))
+    (lyskom-collect 'blocking)
+    (while call-list
+      (apply (intern-soft (concat "initiate-"
+				  (symbol-name (car (car call-list)))))
+	     'blocking nil
+	     (cdr (car call-list)))
+      (setq call-list (cdr call-list)))
+    (lyskom-use 'blocking 'lyskom-blocking-do-multiple-1)
+    (while (eq lyskom-multiple-blocking-return 'not-yet-gotten)
+      (accept-process-output))
+    lyskom-multiple-blocking-return))
+
+(defun lyskom-blocking-do-multiple-1 (&rest data)
+  (setq lyskom-multiple-blocking-return data))
+
+(defmacro blocking-do-multiple (bind-list &rest body)
+  "Bind variables according to BIND-LIST and then eval BODY.
+The value of the last form in BODY is returned.
+Each element in BIND-LIST is a list (SYMBOL FORM) which binds SYMBOL to
+the result of the server call FORM, which is the same as used in blocking-do.
+All the forms in BIND-LIST are evaluated before and symbols are bound."
+  (let ((bindsym 'multiple-bind-sym)
+	(index 0))
+    (` (let (((, bindsym)
+	      (lyskom-blocking-do-multiple
+	       (list (,@ (mapcar (function (lambda (x) 
+					     (` (list '(, (car (car (cdr x))))
+						      (,@ (cdr (car (cdr x))))))))
+			   bind-list))))))
+	 (let ((,@ (mapcar (function 
+			    (lambda (bpat)
+			      (prog1
+				  (` ((, (car bpat))
+				      (elt (, bindsym) (, index))))
+				(setq index (1+ index)))))
+			   bind-list)))
+	   (,@ body))))))
 
 
 ;;; Local Variables: 
