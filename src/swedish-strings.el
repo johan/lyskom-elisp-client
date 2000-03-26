fe8fa2c2bdb7cc44fed3a7cebd2bef945d1e6f41
David Byers <byers@lysator.liu.se>
2000-03-26 17:13:26 +0000

    Fixed oops in swedish-strings wrt kom-morons in customize buffer

--- src/swedish-strings.el
+++ src/swedish-strings.el
@@ -1,6 +1,6 @@
 ;;;;; -*-coding: iso-8859-1;-*-
 ;;;;;
-;;;;; $Id: swedish-strings.el,v 44.107 2000-03-26 13:30:54 byers Exp $
+;;;;; $Id: swedish-strings.el,v 44.108 2000-03-26 17:13:26 byers Exp $
 ;;;;; Copyright (C) 1991, 1996  Lysator Academic Computer Association.
 ;;;;;
 ;;;;; This file is part of the LysKOM server.
@@ -39,7 +39,7 @@
 
 (setq lyskom-clientversion-long 
       (concat lyskom-clientversion-long
-	      "$Id: swedish-strings.el,v 44.107 2000-03-26 13:30:54 byers Exp $\n"))
+	      "$Id: swedish-strings.el,v 44.108 2000-03-26 17:13:26 byers Exp $\n"))
 
 
 ;;; ================================================================
@@ -2824,7 +2824,7 @@ i servern. Annars sparas det i din .emacs.")
   Namnen på personerna i denna lista kommer att visas med ett speciellt 
   utseende i LysKOM-bufferten.")
 
-    (kom-friends-doc . "\
+    (kom-morons-doc . "\
   Namnen på personerna i denna lista kommer att visas med ett varnande 
   utseende i LysKOM-bufferten.")
 
@@ -3151,7 +3151,7 @@ i servern. Annars sparas det i din .emacs.")
 
     (lyskom-filter-outgoing-messages-tag . "Visa automatiska meddelanden:")
     (kom-friends-tag . "Vänner och bekanta:")
-    (kom-friends-tag . "Idioter och knäppskallar:")
+    (kom-morons-tag . "Idioter och knäppskallar:")
     (kom-url-viewer-preferences-tag . "Öppna URLer med följande program:")
     (kom-mosaic-command-tag . "Kommando för att starta NCSA Mosaic:")
     (kom-netscape-command-tag . "Kommando för att starta Netscape Navigator:")
