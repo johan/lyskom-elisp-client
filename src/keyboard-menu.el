5f20f243b27bd2ed9e56f7115d622dc6c9997b73
David Byers <byers@lysator.liu.se>
2002-06-29 20:29:19 +0000

    Improved tty menus- Got rid of compiler warnings.

--- src/keyboard-menu.el
+++ src/keyboard-menu.el
@@ -1,6 +1,6 @@
 ;;;;; -*-coding: iso-8859-1;-*-
 ;;;;;
-;;;;; $Id: keyboard-menu.el,v 44.1 2002-06-26 20:20:02 byers Exp $
+;;;;; $Id: keyboard-menu.el,v 44.2 2002-06-29 20:29:20 byers Exp $
 ;;;;; Copyright (C) 1991-2002  Lysator Academic Computer Association.
 ;;;;;
 ;;;;; This file is part of the LysKOM Emacs LISP client.
@@ -36,7 +36,7 @@
 
 (setq lyskom-clientversion-long 
       (concat lyskom-clientversion-long
-	      "$Id: keyboard-menu.el,v 44.1 2002-06-26 20:20:02 byers Exp $\n"))
+	      "$Id: keyboard-menu.el,v 44.2 2002-06-29 20:29:20 byers Exp $\n"))
 
 (defvar lyskom-keyboard-menu-menu)
 (defvar lyskom-keyboard-menu-buffer)
@@ -44,7 +44,11 @@
 (defvar lyskom-keyboard-menu-selection)
 (defvar lyskom-keyboard-menu-overlay)
 
+(defvar lyskom-keyboard-menu-keymap nil)
+
 (defun lyskom-keyboard-menu-read-char (prompt)
+  "Read a single keyboard event from the keyboard.
+Like read-event in Gnu Emacs or next-command-event in XEmacs."
   (message prompt)
   (sit-for 0)
   (lyskom-xemacs-or-gnu
@@ -55,7 +59,6 @@
 		    ((event-key ev))
 		    (t nil))))
    (read-event)))
-
 
 (defun lyskom-keyboard-menu-keys-for-string (string)
   "Return a list of suitable mnemonics for menu item STRING."
@@ -122,6 +125,7 @@ same element's cdr."
 
 
 (defun lyskom-keyboard-menu-highlight-selected ()
+  "Highlight the selected menu item"
   (set-buffer lyskom-keyboard-menu-buffer)
   (let ((pos (text-property-any (point-min) 
                                 (point-max)
@@ -133,22 +137,76 @@ same element's cdr."
             (set-extent-face lyskom-keyboard-menu-overlay nil)
             (overlay-put lyskom-keyboard-menu-overlay 'face nil))
            (goto-char (point-min)))
-          (t (lyskom-xemacs-or-gnu
-              (set-extent-endpoints lyskom-keyboard-menu-overlay
-                                    pos
-                                    (save-excursion (goto-char pos)
-                                                    (end-of-line)
-                                                    (1+ (point))))
-              (move-overlay lyskom-keyboard-menu-overlay
-                            pos
-                            (save-excursion (goto-char pos)
-                                            (end-of-line)
-                                            (1+ (point)))))
+          (t (let ((start pos)
+                   (end (or (next-single-property-change 
+                             pos
+                             'lyskom-keyboard-menu-item)
+                            (point-max))))
+               (lyskom-xemacs-or-gnu
+                (set-extent-endpoints lyskom-keyboard-menu-overlay
+                                      start end)
+                (move-overlay lyskom-keyboard-menu-overlay
+                              start end)))
              (goto-char pos)
              (lyskom-xemacs-or-gnu
               (set-extent-face lyskom-keyboard-menu-overlay 'kom-mark-face)
               (overlay-put lyskom-keyboard-menu-overlay
-                           'face 'kom-mark-face))))))
+                           'face 'kom-mark-face))))
+    (save-excursion
+      (unless (pos-visible-in-window-p pos)
+        (recenter)))))
+
+
+(defun lyskom-keyboard-menu-format-item (item &optional pad-len)
+  "Format a single menu item as a string"
+  (let ((string (format "%s (%s)" (elt item 1) (elt item 0))))
+    (when (and pad-len (> pad-len (length string)))
+      (setq string (concat string (make-string (- pad-len (length string))
+                                               ?\ ))))
+    (add-text-properties 0 
+                         (length string)
+                         (list 'lyskom-keyboard-menu-item item)
+                         string)
+    string))
+
+(defun lyskom-keyboard-menu-insert (prompt menu)
+  "Insert the text-based representation of the menu.
+PROMPT is the menu title and MENU is the menu data."
+  (let* ((line-length (apply 'max
+                             (mapcar (lambda (x)
+                                       (+ (length (elt x 0))
+                                          (length (elt x 1))
+                                          3))
+                                     menu)))
+         (columns (/ (+ (window-width) 1)
+                     (+ line-length 2)))
+         (dashes (make-string (+ (* line-length columns)
+                                 (* 2 (1- columns))) ?-))
+         (quit-item nil))
+
+    (insert (format "%s\n%s\n" prompt dashes))
+    (lyskom-traverse el menu
+      (if (null (elt el 2))
+          (setq quit-item el)
+        (let ((string (lyskom-keyboard-menu-format-item el line-length)))
+          (when (> (+ (current-column) (length string) 2) (window-width))
+            (insert "\n"))
+          (unless (bolp) (insert "  "))
+          (insert string))))
+    (unless (bolp)
+      (insert "\n"))
+    (insert dashes)
+    (insert "\n")
+    (when quit-item
+      (insert (lyskom-keyboard-menu-format-item quit-item)))))
+
+
+
+
+;;; ================================================================
+;;; Interactive commands
+;;; 
+
 
 (defun lyskom-read-from-menu (prompt table)
   "Let the user select one of the values in TABLE.
@@ -156,40 +214,27 @@ TABLE is an alist whose elements' cars are strings.
 Returns the selected string."
   (let* ((menu (lyskom-keyboard-menu-frob-table table))
          (buffer (get-buffer-create "*Keyboard menu*"))
-         (line-length (apply 'max
-                             (mapcar (lambda (x)
-                                       (+ (length (elt x 0))
-                                          (length (elt x 1))
-                                          3))
-                                     menu))))
+         (menu-window-height 0))
     (save-excursion
 
       ;; Format the buffer
 
       (set-buffer buffer)
       (erase-buffer)
-      (insert (format "%s\n%s\n"
-                      prompt
-                      (make-string line-length ?-)))
-      (lyskom-traverse el menu
-        (unless (cdr (memq el menu))
-          (insert (make-string line-length ?-))
-          (insert "\n"))
-        (let ((start (point)))
-          (insert (format "%s (%s)\n" (elt el 1) (elt el 0)))
-          (add-text-properties start
-                               (point)
-                               (list 'lyskom-keyboard-menu-item el)
-                               )))
-      (goto-char (point-min))
+      (lyskom-keyboard-menu-insert prompt menu)
 
-      (setq table (cons (cons nil nil) table))
+      (goto-char (point-min))
+      (setq menu-window-height (+ 2 (count-lines (point-min) (point-max))))
 
       ;; Display the window and do the thing
 
       (save-window-excursion
-        (split-window (selected-window) (+ 5 (length table)))
-        (switch-to-buffer buffer)
+        (if (< (- (window-height (selected-window))
+                  menu-window-height) 5)
+            (select-window
+             (display-buffer buffer t))
+          (split-window (selected-window) menu-window-height)
+          (switch-to-buffer buffer))
         (let* ((lyskom-keyboard-menu-buffer buffer)
                (lyskom-keyboard-menu-menu menu)
                (lyskom-keyboard-menu-selection nil)
@@ -214,45 +259,153 @@ Returns the selected string."
                                              "")))))
                   (cond ((eq (lookup-key global-map (vector c)) 'keyboard-quit)
                          (keyboard-quit))
+                        ((eq c ?\C-a) (lyskom-keyboard-menu-beginning-of-line))
+                        ((eq c ?\C-e) (lyskom-keyboard-menu-end-of-line))
                         ((or (eq c 'up)
-			     (eq c ?\C-p)
-			     (eq c ?\C-b)
-			     (eq c 'left))
+                             (eq c ?\C-p))
                          (lyskom-keyboard-menu-up))
+                        ((or (eq c ?\C-b)
+                             (eq c 'left))
+                         (lyskom-keyboard-menu-backward))
                         ((or (eq c 'down)
-			     (eq c ?\C-n)
-			     (eq c ?\C-f)
-			     (eq c 'right))
+                             (eq c ?\C-n))
                          (lyskom-keyboard-menu-down))
+                        ((or (eq c ?\C-f)
+                             (eq c 'right))
+                         (lyskom-keyboard-menu-forward))
                         ((or (eq c 'return)
-			     (eq c ?\r)
-			     (eq c ?\n))
+                             (eq c ?\r)
+                             (eq c ?\n))
                          (setq result lyskom-keyboard-menu-selection))
                         ((and (characterp c)
                               (assoc (make-string 1 c) 
-				     lyskom-keyboard-menu-menu))
+                                     lyskom-keyboard-menu-menu))
                          (if kom-keyboard-menu-immediate-selection
                              (setq result (assoc (make-string 1 c) 
-						 lyskom-keyboard-menu-menu))
+                                                 lyskom-keyboard-menu-menu))
                            (setq lyskom-keyboard-menu-selection
                                  (assoc (make-string 1 c)
-					lyskom-keyboard-menu-menu))))))
+                                        lyskom-keyboard-menu-menu))))))
                 (when lyskom-keyboard-menu-selection
                   (setq lyskom-keyboard-menu-menu
                         (lyskom-rotate-list lyskom-keyboard-menu-menu
                                             lyskom-keyboard-menu-selection))))
             (quit (setq result nil)))
           (elt result 2)
-      )))))
-
+          )))))
 
 (defun lyskom-keyboard-menu-up ()
+  "Move up one menu item"
   (interactive)
-  (setq lyskom-keyboard-menu-selection (car (last lyskom-keyboard-menu-menu))))
+  (let ((done nil))
+    (while (not done)
 
+      ;; Move up one line
+      (condition-case nil
+          (previous-line 1)
+        (beginning-of-buffer (setq done t)))
+
+      ;; Scan the items on this line until we find the one
+      ;; just before point or just after point or around
+      ;; point.
+
+      (let ((selection nil)
+            (start (save-excursion (beginning-of-line) (point)))
+            (end (save-excursion (end-of-line) (point)))
+            (pos (point)))
+        (while (and (>= pos start) (null selection))
+          (setq selection 
+                (get-text-property pos 'lyskom-keyboard-menu-item)
+                pos (1- pos)))
+
+        (setq pos (point))
+        (while (and (<= pos end) (null selection))
+          (setq selection 
+                (get-text-property pos 'lyskom-keyboard-menu-item)
+                pos (1+ pos)))
+        (when selection (setq done t
+                              lyskom-keyboard-menu-selection selection))))))
 
 (defun lyskom-keyboard-menu-down ()
+  "Move down one menu item."
   (interactive)
-  (if lyskom-keyboard-menu-selection
-      (setq lyskom-keyboard-menu-selection (car (cdr lyskom-keyboard-menu-menu)))
-    (setq lyskom-keyboard-menu-selection (car lyskom-keyboard-menu-menu))))
+  (let ((done nil))
+    (while (not done)
+
+      ;; Move up one line
+      (condition-case nil
+          (next-line 1)
+        (end-of-buffer (setq done t)))
+
+      ;; Scan the items on this line until we find the one
+      ;; just before point or just after point or around
+      ;; point.
+
+      (let ((selection nil)
+            (start (save-excursion (beginning-of-line) (point)))
+            (end (save-excursion (end-of-line) (point)))
+            (pos (point)))
+        (while (and (>= pos start) (null selection))
+          (setq selection 
+                (get-text-property pos 'lyskom-keyboard-menu-item)
+                pos (1- pos)))
+
+        (setq pos (point))
+        (while (and (<= pos end) (null selection))
+          (setq selection 
+                (get-text-property pos 'lyskom-keyboard-menu-item)
+                pos (1+ pos)))
+        (when selection (setq done t
+                              lyskom-keyboard-menu-selection selection))))))
+
+
+
+
+(defun lyskom-keyboard-menu-forward ()
+  "Move right one menu item."
+  (interactive)
+  (let ((pos (point))
+        (item nil))
+    (while (and pos (or (null item) (eq item lyskom-keyboard-menu-selection)))
+      (setq pos (next-single-property-change pos 'lyskom-keyboard-menu-item))
+      (when pos
+        (setq item (get-text-property pos 'lyskom-keyboard-menu-item))))
+    (when item (setq lyskom-keyboard-menu-selection item))))
+
+
+(defun lyskom-keyboard-menu-backward ()
+  "Move left one menu item."
+  (interactive)
+  (let ((pos (point))
+        (item nil))
+    (while (and pos (or (null item) (eq item lyskom-keyboard-menu-selection)))
+      (setq pos (previous-single-property-change pos 'lyskom-keyboard-menu-item))
+      (when pos
+        (setq item (get-text-property pos 'lyskom-keyboard-menu-item))))
+    (when item (setq lyskom-keyboard-menu-selection item))))
+
+(defun lyskom-keyboard-menu-beginning-of-line ()
+  "Move to the first menu item of this line."
+  (interactive)
+  (beginning-of-line)
+  (setq lyskom-keyboard-menu-selection
+        (or (get-text-property (point) 'lyskom-keyboard-menu-item)
+            (get-text-property (or (next-single-property-change 
+                                    (point) 'lyskom-keyboard-menu-item
+                                    nil (save-excursion (end-of-line) (point)))
+                                   (point))
+                               'lyskom-keyboard-menu-item))))
+
+(defun lyskom-keyboard-menu-end-of-line ()
+  "Move to the last menu item of this line."
+  (interactive)
+  (end-of-line)
+  (setq lyskom-keyboard-menu-selection
+        (if (get-text-property (point) 'lyskom-keyboard-menu-item)
+            (get-text-property (point) 'lyskom-keyboard-menu-item)
+          (let ((pos (previous-single-property-change 
+                      (point) 'lyskom-keyboard-menu-item
+                      nil (1- (save-excursion (beginning-of-line) (point))))))
+            (when (and pos (not (zerop pos)))
+              (or (get-text-property pos 'lyskom-keyboard-menu-item)
+                  (get-text-property (1- pos)  'lyskom-keyboard-menu-item)))))))
